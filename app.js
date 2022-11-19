969920ab7defd8351166dae8383cd15bbdf421bd
gbabuadze <gbabuadze1@gmail.com>
2022-11-19 14:29:00 +0400

    two-way-binding

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  data() {
+    return { userInput: "", confirmedInput: "" };
+  },
+  methods: {
+    showAlert() {
+      alert("Hello !");
+    },
+    saveInput(event) {
+      this.userInput = event.target.value;
+    },
+    confirmInput() {
+      this.confirmedInput = this.userInput;
+    },
+  },
+});
+
+app.mount("#assignment");
